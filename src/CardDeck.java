f6e978c58e2a20a192b09f1012f44f6384681dee
fliod <kgraceaz@gmail.com>
2015-01-20 01:52:19 -0700

    Update CardDeck.java

--- src/CardDeck.java
+++ src/CardDeck.java
@@ -1,30 +1,30 @@
 // create class CardDeck that extends CardList
-// create class CardDeck that extends CardList
 import java.util.*;
 public class CardDeck extends CardList
 {
   private Random random=new Random();
-  private int suits;
-  private int ranks;
-  private int swaps;
+  private int suits=4;
+  private int ranks=13;
+  private int swaps=10000;
   public CardDeck()
   {
     super();
-  }
-  public ArrayList shuffle(ArrayList cards)
-  {
-    int r;
-    int x;
-    ArrayList out=cards;
-    Card temp;
-    for(int i=0;i<10000;i++)
+    for(int i=1;i<=4;i++)
     {
-      r=random.nextInt(cards.size());
-      x=random.nextInt(cards.size());
-      temp=(Card)out.get(r);
-      out.set(r,out.get(x));
-      out.set(x,temp);
+      for(int x=1;x<=13;x++)
+      {
+        Card tempCard=new Card(i,x);
+        this.addCardToTop(tempCard);
+      }
     }
-    return out;
+  }
+  public void shuffle()
+  {
+    Card temp;
+    for(int i=0;i<swaps;i++)
+    {
+      temp=this.removeRandomCard();
+      this.addCardToTop(temp);
+    }
   }
 }
